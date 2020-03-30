4c207eda1d3ee0ca678b41becb7488e71453d1ba
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2020-03-30 13:33:49 +0200

    Initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,50 @@
+﻿namespace BankV10
+{
+    public class BankAccount
+    {
+        #region Instance fields
+        private double _balance;
+        #endregion
+
+        #region Constructor
+        public BankAccount()
+        {
+            _balance = 0.0;
+        }
+        #endregion
+
+        #region Properties
+        public double Balance
+        {
+            get { return _balance; }
+        }
+        #endregion
+
+        #region Methods
+        public void Deposit(double amount)
+        {
+            if(amount <= 0)
+            {
+                System.Console.WriteLine("Please enter a positive number");
+            }else
+            {
+                _balance = _balance + amount;
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if(amount <= 0){
+                System.Console.WriteLine("Please enter a positive number");
+            }
+            else if(amount <= _balance)
+            {
+                _balance = _balance - amount;
+            }else
+            {
+                System.Console.WriteLine("You do not have that much money bitch");
+            }
+        } 
+        #endregion
+    }
+}
